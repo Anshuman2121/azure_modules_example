2db4dd86f17fd1372968375844d1525f9a7158bf
Anshuman Abhishek <anshuman2121@gmail.com>
2023-05-03 23:52:24 +0200

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -1,3 +1,8 @@
+resource "azurerm_resource_group" "myResourceGroup" {
+  name     = "myResourceGroup"
+  location = "West Europe"
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
   address_space       = [var.address_space]
@@ -11,4 +16,5 @@ resource "azurerm_virtual_network" "vnet" {
       address_prefix = subnet.value.prefix
     }
   }
+  depends_on = [ azurerm_resource_group.myResourceGroup ]
 }
